4cbdb9afb423f254f8b5285b4b4996c43ffdd362
Anwer Baccar <anwer.baccar.pro@gmail.com>
2022-02-18 16:53:29 +0100

    add midlewares security

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const objectRoutes = require('./routes/objectRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -11,12 +16,34 @@ const app = express();
 
 app.use(cors());
 
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+app.use(helmet());
+
+if (process.env.USER === "development") {
+  app.use(morgan("dev"));
 }
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 300,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: [
+      "name",
+    ],
+  })
+);
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/objects', objectRoutes);
